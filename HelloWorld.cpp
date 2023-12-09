037b6229b545c91876f9783bc9d955caff11434e
chung1504 <nguyenvietchung150404@gmail.com>
2023-12-09 13:36:54 +0700

    Update

---
+++ HelloWorld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void hello() {
+    cout << "My name is Chung"; 
+}
+
+int main () {
+    cout << "Hello"; 
+    hello(); 
+}
